a9681d3f8eefbfc993987bf3d5da485311d7068b
axell <mail@axell.me>
2023-12-06 20:40:08 +0100

    Update snippets.lua

--- snippets.lua
+++ snippets.lua
@@ -1,2 +1,2 @@
---Change all trees leaves to white for christmas
+--Change all trees leaves and the baseplate to white for christmas
 game:GetService("ChangeHistoryService"):TryBeginRecording("snowadded", "added snow") for i,obj in pairs(workspace:GetDescendants()) do if obj.Name == "Leaves" then obj.BrickColor = BrickColor.White() end end game:GetService("ChangeHistoryService"):FinishRecording("snowadded", Enum.FinishRecordingOperation.Append)
